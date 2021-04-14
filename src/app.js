2f1f2eca73251b04dc233c26e0f224df88bfd3dd
scoobidou <emmanuel.croccel@outlook.fr>
2021-04-14 19:42:51 +0200

    File Handling and Routing - checking code

--- src/app.js
+++ src/app.js
@@ -4,16 +4,39 @@ const express = require ('express');
 
 const port = 3000;
 
+const accountData = fs.readFileSync(path.join(__dirname,'/json/accounts.json'), "utf-8");
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname,'/json/users.json'), "utf-8");
+const users = JSON.parse(userData);
+
 const app = express();
 
 app.set( 'view engine' , 'ejs');
-app.set( 'views', path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'public')));
+app.set( 'views', path.join(__dirname,'/views'));
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/', function(req,res){
-    res.render('index', { title: "Index" });
+    res.render('index', { title: "Account Summary" ,accounts: accounts});
+});
+
+app.get('/savings', function(req,res){
+    res.render('account',{account: accounts.savings});
+});
+
+app.get('/checking', function(req,res){
+    res.render('account',{account: accounts.checking});
+});
+
+app.get('/credit', function(req,res){
+    res.render('account',{account: accounts.credit});
+});
+
+app.get('/profile', function(req,res){
+    res.render('profile',{user: users[0]});
 });
 
 app.listen(port, () => {
     console.log(`PS Project Running on port ${port}!`);
 })
+
