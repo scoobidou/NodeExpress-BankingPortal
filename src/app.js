e2e5fb6cd4ea78eb2e5b8e94b082fcdce8a5051c
scoobidou <emmanuel.croccel@outlook.fr>
2021-04-14 20:12:50 +0200

    Handling Form Data - checking work

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.set( 'view engine' , 'ejs');
 app.set( 'views', path.join(__dirname,'/views'));
 app.use(express.static(path.join(__dirname,'/public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function(req,res){
     res.render('index', { title: "Account Summary" ,accounts: accounts});
 });
@@ -34,6 +36,29 @@ app.get('/credit', function(req,res){
 
 app.get('/profile', function(req,res){
     res.render('profile',{user: users[0]});
+});
+
+app.get('/transfer', function(req,res){
+    res.render('transfer');
+});
+
+app.get('/payment', function(req,res){
+    res.render('payment', {account: accounts.credit});
+});
+
+app.post('/payment',function(req,res){
+    accounts.credit.balance -= parseFloat(req.body.amount);
+    accounts.credit.available += parseFloat(req.body.amount);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),JSON.stringify.accounts,'utf-8');
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
+
+app.post('/transfer',function(req,res){
+    accounts[req.body.from].balance -= parseFloat(req.body.amount);
+    accounts[req.body.to].balance += parseFloat(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf-8');
+    res.render('transfer', {message: "Transfer Completed"});
 });
 
 app.listen(port, () => {
